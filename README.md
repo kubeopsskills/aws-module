8a34ed97b0779e8179e096b2620f41dfb3341665
saritratj <saritrat@kubeops.guru>
2021-06-10 20:38:12 +0700

    init aws module

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # aws-module
+
+aws
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+
+eksctl
+https://github.com/mumoshu/terraform-provider-eksctl
---
+++ network/vpc/variables.tf
@@ -0,0 +1,8 @@
+variable "cidr_block" {
+  description = "The CIDR block for the VPC."
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resource."
+  type = map(string)
+}
