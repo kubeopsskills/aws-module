e91a11913631da8dc16c3b43079f5af6c6edcb59
saritratj <saritrat@kubeops.guru>
2021-07-01 01:18:40 +0700

    module: add eip

---
+++ network/nat-gateway/variables.tf
@@ -0,0 +1,12 @@
+variable "vpc_id" {
+  description = "The VPC ID."
+}
+
+variable "route" {
+  description = "A list of route objects. Their keys are documented below. This argument is processed in attribute-as-blocks mode."
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resource."
+  type = map(string)
+}
