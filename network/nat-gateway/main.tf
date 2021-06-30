158708119359cfe8ab8d410beab24a8980c02dfa
saritratj <saritrat@kubeops.guru>
2021-07-01 03:40:14 +0700

    network: adjust nat gateway

--- network/nat-gateway/main.tf
+++ network/nat-gateway/main.tf
@@ -1,4 +1,4 @@
-resource "aws_nat_gateway" "example" {
+resource "aws_nat_gateway" "nat_gw" {
   subnet_id            = var.subnet_id
   allocation_id        = var.allocation_id
   connectivity_type    = var.connectivity_type
