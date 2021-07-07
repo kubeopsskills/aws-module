f2ed32a724ffae9c4e6fb66cce18bbdaee0aad73
saritratj <saritrat@kubeops.guru>
2021-07-07 15:30:56 +0700

    security: adjust security group module

--- security/security-group/main.tf
+++ security/security-group/main.tf
@@ -1,24 +1,48 @@
 resource "aws_security_group" "security_group" {
-  name               = var.name
-  description        = var.description
-  vpc_id             = var.vpc_id
-  ingress            = {
-    description      = var.description
-    from_port        = var.from_port
-    to_port          = var.to_port
-    protocol         = var.protocol
-    security_groups  = var.security_groups
-    cidr_blocks      = var.cidr_blocks
-    ipv6_cidr_blocks = var.ipv6_cidr_blocks
+  name        = var.name
+  description = var.description
+  vpc_id      = var.vpc_id
+  dynamic "ingress" {
+    for_each = [for i in var.ingresses : {
+      description      = i.description
+      from_port        = i.from_port
+      to_port          = i.to_port
+      protocol         = i.protocol
+      security_groups  = i.security_groups
+      cidr_blocks      = i.cidr_blocks
+      ipv6_cidr_blocks = i.ipv6_cidr_blocks
+    }]
+    content {
+      description      = ingress.value.description
+      from_port        = ingress.value.from_port
+      to_port          = ingress.value.to_port
+      protocol         = ingress.value.protocol
+      security_groups  = ingress.value.security_groups
+      cidr_blocks      = ingress.value.cidr_blocks
+      ipv6_cidr_blocks = ingress.value.ipv6_cidr_blocks
+    }
   }
-  egress             = {
-    description      = var.description
-    from_port        = var.from_port
-    to_port          = var.to_port
-    protocol         = var.protocol
-    security_groups  = var.security_groups
-    cidr_blocks      = var.cidr_blocks
-    ipv6_cidr_blocks = var.ipv6_cidr_blocks
+
+  dynamic "egress" {
+    for_each = [for e in var.egresses : {
+      description      = e.description
+      from_port        = e.from_port
+      to_port          = e.to_port
+      protocol         = e.protocol
+      security_groups  = e.security_groups
+      cidr_blocks      = e.cidr_blocks
+      ipv6_cidr_blocks = e.ipv6_cidr_blocks
+    }]
+    content {
+      description      = egress.value.description
+      from_port        = egress.value.from_port
+      to_port          = egress.value.to_port
+      protocol         = egress.value.protocol
+      security_groups  = egress.value.security_groups
+      cidr_blocks      = egress.value.cidr_blocks
+      ipv6_cidr_blocks = egress.value.ipv6_cidr_blocks
+    }
   }
-  tags          = var.tags
+
+  tags = var.tags
 }
--- security/security-group/variables.tf
+++ security/security-group/variables.tf
@@ -1,23 +1,44 @@
 variable "name" {
   description = "Name of the security group."
+  type = string
 }
 
 variable "description" {
   description = "Security group description."
+  type = string
 }
 
 variable "vpc_id" {
   description = "VPC ID."
+  type = string
 }
-variable "ingress" {
-  description = "The AZ for the subnet."
+variable "ingresses" {
+  description = "Ingress Rules"
+  type = list(object({
+    description = string,
+    from_port = string,
+    to_port = string,
+    protocol = string,
+    security_groups = list(string),
+    cidr_blocks = list(string),
+    ipv6_cidr_blocks = list(string)
+  }))
 }
-variable "egress" {
-  description = "The AZ for the subnet."
+variable "egresses" {
+  description = "Egress Rules"
+  type = list(object({
+    description = string,
+    from_port = string,
+    to_port = string,
+    protocol = string,
+    security_groups = list(string),
+    cidr_blocks = list(string),
+    ipv6_cidr_blocks = list(string)
+  }))
 }
 
 
 variable "tags" {
   description = "A map of tags to assign to the resource."
-  type = map(string)
+  type        = map(string)
 }
