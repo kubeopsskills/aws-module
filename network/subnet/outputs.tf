cad0b08e76b67820c88535c4391d2e0fac776d3a
dmakeroam <cpfapp@gmail.com>
2021-07-26 14:01:56 +0700

    add: subnet_name output to [network/subnet]

--- network/subnet/outputs.tf
+++ network/subnet/outputs.tf
@@ -2,3 +2,8 @@ output "subnet_id" {
     description = "Subnet ID"
     value = aws_subnet.subnet.id
 }
+
+output "subnet_name" {
+    description = "Subnet Name"
+    value = aws_subnet.subnet.tags["Name"]
+}
