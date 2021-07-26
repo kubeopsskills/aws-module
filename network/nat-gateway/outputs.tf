9a0a4f6ca010b770471048435c50abb1244df5fd
dmakeroam <cpfapp@gmail.com>
2021-07-26 21:17:31 +0700

    add: output nat_gw_name to [network/nat-gateway]

--- network/nat-gateway/outputs.tf
+++ network/nat-gateway/outputs.tf
@@ -2,3 +2,8 @@ output nat_gw_id {
     description = "NAT Gateway ID"
     value = aws_nat_gateway.nat_gw.id
 }
+
+output nat_gw_name {
+    description = "NAT Gateway Name"
+    value = aws_nat_gateway.nat_gw.tags["Name"]
+}
