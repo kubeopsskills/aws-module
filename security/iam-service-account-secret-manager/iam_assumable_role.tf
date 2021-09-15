858bc821174ed390256f6a3465bf5602d72d44bd
saritratj <saritrat@kubeops.guru>
2021-09-15 18:13:43 +0700

    fix: adjust addition policy

--- security/iam-service-account-secret-manager/iam_assumable_role.tf
+++ security/iam-service-account-secret-manager/iam_assumable_role.tf
@@ -5,7 +5,7 @@ module "iam_assumable_role" {
   role_name        = var.role_name
   tags             = var.tags
   provider_url     = replace(var.cluster_oidc_issuer_url, "https://", "")
-  role_policy_arns = var.enabled_additional_policy ? [aws_iam_policy.secret_manager.arn, aws_iam_policy.additional_policy[count.index].arn] : [aws_iam_policy.secret_manager.arn]
+  role_policy_arns = var.enabled_additional_policy ? [aws_iam_policy.secret_manager.arn, aws_iam_policy.additional_policy[0].arn] : [aws_iam_policy.secret_manager.arn]
 
   oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:${var.service_account_name}"]
   depends_on                    = [kubernetes_service_account.service_account]
